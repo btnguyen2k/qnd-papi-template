55bff6a207c8640d9d422260e4bad3788e101b22
CI Build <>
2026-02-04 13:24:51 +0000

    Update RELEASE-NOTES.md, *.md files and version info in app/main.py for new version 0.1.0

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 # from .config import settings
 from .routers import demo
 
-VERSION = "0.0.0"
+VERSION = "0.1.0"
 APP_NAME = "Hello World API"
 APP_DESCRIPTION = "A simple Hello World API server"
 
